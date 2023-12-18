c4fff3b677e37f3b8e4e4a8b83116e8acc45842a
madiocre <wolvirex@gmail.com>
2023-12-18 23:31:31 +0200

    added insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,19 +1,46 @@
 #include "sort.h"
 
 /**
+ * sortedInsert - Sorts using insert method.
+ * @sorted: The sorted.
+ * @next: The next node.
+ * @prev: The prev node.
+ */
+void sortedInsert(listint_t **head, listint_t **prev, listint_t *current)
+{
+    (*prev)->next = current->next;
+    if (current->next != NULL)
+        current->next->prev = (*prev);
+    current->prev = (*prev)->prev;
+    current->next = (*prev);
+    if((*prev)->prev != NULL){
+        (*prev)->prev->next = current;
+    }
+    else
+        (*head) = current;
+    (*prev)->prev = current;
+    (*prev) = current->prev;
+
+}
+
+/**
  * insertion_sort_list - Sorts a list of integers
  *
  * @@list: The list to be sorted
  */
 
 void insertion_sort_list(listint_t **list){
-    listint_t *sorted = NULL;
-    listint_t *current = *list;
+    listint_t *current, *next, *prev;
 
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+    current = (*list)->next;
     while (current != NULL) {
-        listint_t *next = current->next;
-        sorted = sortedInsert(sorted, current);
+        next = current->next;
+        prev = current->prev;
+        while(prev != NULL && current->n < prev->n)
+            sortedInsert(list, &prev, current);
+        print_list((const listint_t *)*list);
         current = next;
     }
-    *list = sorted;
 }
--- sort.h
+++ sort.h
@@ -4,6 +4,17 @@
 #include <stdlib.h>
 
 /**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true = 1
+} bool;
+
+/**
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
@@ -12,9 +23,9 @@
  */
 typedef struct listint_s
 {
-const int n;
-struct listint_s *prev;
-struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
